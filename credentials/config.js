0a7707caec2ac7d6061adf4ace980ebfcb0d0299
Namaca <48693912+namaca@users.noreply.github.com>
2023-02-09 12:26:38 -0300

    Update and rename configexemple.js to config.js

---
+++ credentials/config.js
@@ -0,0 +1,14 @@
+const PASSWORD_OBOTE = '' 
+const PASSWORD_MYSQL = '' 
+const client_secret = '' 
+const client_id = ''
+const supinic  = ''
+const token_ghiletofar = ''
+
+module.exports = { PASSWORD_MYSQL, PASSWORD_OBOTE, client_secret, client_id, token_ghiletofar }
+
+
+
+
+
+
